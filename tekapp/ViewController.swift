055c5b1ca7c9618c9ce3702e1b629ffa6ea8d5fc
Önder ADA <103576101+onderadaidenfit@users.noreply.github.com>
2023-10-24 18:50:59 +0300

    Initial Commit

---
+++ tekapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tekapp
+//
+//  Created by Önder Ada on 24.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
